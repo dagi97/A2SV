b3c4a0d057599e5d0ce1a0f396381a6019942e6e
Dagmawit Sisay <145625800+dagi97@users.noreply.github.com>
2025-06-12 21:03:22 +0300

    Created 'Reverse Words in a String' by A2SV Hub

---
+++ 12-Jun-2025/Reverse Words in a String 366008.py
@@ -0,0 +1,5 @@
+# Problem: Reverse Words in a String - https://leetcode.com/problems/reverse-words-in-a-string/
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return " ".join(reversed(s.split()))
