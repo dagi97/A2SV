6b69269f969148018dc21e412625ad3c24164f10
Dagmawit Sisay <145625800+dagi97@users.noreply.github.com>
2025-09-07 19:25:02 +0300

    Created 'Subsets' by A2SV Hub

---
+++ 07-Sep-2025/Subsets 391781.py
@@ -0,0 +1,20 @@
+# Problem: Subsets - https://leetcode.com/problems/subsets/
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+    
+        ans = []
+        
+        def backtrack(start, path):
+           
+            ans.append(path[:])
+            
+            for i in range(start, len(nums)):
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()  # undo choice
+        
+        backtrack(0, [])
+        return ans
+
+        
