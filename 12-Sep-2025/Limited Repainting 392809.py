f10173751449da274818f799b4949273495b0f65
Dagmawit Sisay <145625800+dagi97@users.noreply.github.com>
2025-09-12 20:09:13 +0300

    Created 'Limited Repainting' by A2SV Hub

---
+++ 12-Sep-2025/Limited Repainting 392809.py
@@ -0,0 +1,27 @@
+# Problem: Limited Repainting - https://codeforces.com/contest/2070/problem/C
+
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    s = input()
+    arr = list(map(int, input().split()))
+
+    def isvalid(mid):
+        a = "R"
+        c = 0
+        for i in range(n):
+            if arr[i] > mid:
+                if s[i] == "B" and a == "R":
+                    c += 1
+                a = s[i]
+        return c <= k
+    low = 0
+    high = max(arr)
+
+    while low <= high:
+            mid = (low+high) // 2
+            if isvalid(mid):
+                high = mid-1
+            else:
+                low = mid + 1
+    print(low)
+    
