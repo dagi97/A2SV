4d99c46aa5adf932c97b2dea066b6e2e40879a42
Dagmawit Sisay <145625800+dagi97@users.noreply.github.com>
2025-10-09 19:34:21 +0300

    Created 'Row GCD' by A2SV Hub

---
+++ 09-Oct-2025/Row GCD 401556.py
@@ -0,0 +1,17 @@
+# Problem: Row GCD - https://codeforces.com/problemset/problem/1458/a
+
+import math
+n, k = map(int, input().split())
+a = list( map(int, input().split()))
+b = list( map(int, input().split()))
+check = 0
+
+for i in a:
+    check = math.gcd(check, abs(i-a[0]))
+
+ans = []
+
+for j in b:
+    curr = math.gcd(a[0] + j, check)
+    ans.append(curr)
+print(*ans)
