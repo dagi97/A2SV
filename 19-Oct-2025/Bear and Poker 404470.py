7a54e7e595c2cfcd0f3304320e1bd80f9e03d4c0
Dagmawit Sisay <145625800+dagi97@users.noreply.github.com>
2025-10-19 21:29:20 +0300

    Created 'Bear and Poker' by A2SV Hub

---
+++ 19-Oct-2025/Bear and Poker 404470.py
@@ -0,0 +1,15 @@
+# Problem: Bear and Poker - https://codeforces.com/problemset/problem/574/C
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+for i in range(n):
+    while not arr[i] % 2 or not arr[i] % 3:
+        if not arr[i] % 2:
+            arr[i] //= 2
+        if not arr[i] % 3:
+            arr[i] //= 3
+if len(set(arr)) == 1:
+    print('Yes')
+else:
+    print('No')
