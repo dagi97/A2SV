d2d1e0383d252228b857dc75bcb403ed6633eefa
Dagmawit Sisay <145625800+dagi97@users.noreply.github.com>
2025-11-11 22:40:47 +0300

    Created 'Serval and The Formula' by A2SV Hub

---
+++ 11-Nov-2025/Serval and The Formula 408483.py
@@ -0,0 +1,9 @@
+# Problem: Serval and The Formula - https://codeforces.com/problemset/problem/2085/C
+
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+
+    if a == b:
+        print(-1)
+    else:
+        print((1 << 30) - max(a, b))
