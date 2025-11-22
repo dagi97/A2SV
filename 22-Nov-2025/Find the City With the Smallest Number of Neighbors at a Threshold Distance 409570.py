a417e6fce7670a1b538a37ea866dce067b20cb66
Dagmawit Sisay <145625800+dagi97@users.noreply.github.com>
2025-11-22 19:23:04 +0300

    Created 'Find the City With the Smallest Number of Neighbors at a Threshold Distance' by A2SV Hub

---
+++ 22-Nov-2025/Find the City With the Smallest Number of Neighbors at a Threshold Distance 409570.py
@@ -0,0 +1,26 @@
+# Problem: Find the City With the Smallest Number of Neighbors at a Threshold Distance - https://leetcode.com/problems/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/
+
+class Solution:
+    def findTheCity(self, n: int, edges: List[List[int]], t: int) -> int:
+        dist = [[float("inf")] * n for _ in range(n)]
+        for i in range(n):
+            dist[i][i] = 0
+
+        for u, v, w in edges:
+            dist[u][v] = w
+            dist[v][u] = w
+
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    if dist[i][j] > dist[i][k] + dist[k][j]:
+                        dist[i][j] = dist[i][k] + dist[k][j]
+
+        ans = -1
+        best = n
+        for i in range(n):
+            count = sum(1 for j in range(n) if i != j and dist[i][j] <= t)
+            if count <= best:
+                best = count
+                ans = i
+        return ans
