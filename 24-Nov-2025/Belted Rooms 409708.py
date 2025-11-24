558b908ec5c21cd64fc604d1292c37e2b1b740b5
Dagmawit Sisay <145625800+dagi97@users.noreply.github.com>
2025-11-24 18:35:16 +0300

    Created 'Belted Rooms' by A2SV Hub

---
+++ 24-Nov-2025/Belted Rooms 409708.py
@@ -0,0 +1,14 @@
+# Problem: Belted Rooms - https://codeforces.com/problemset/problem/1428/B
+
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    ans = set()
+    if "<" not in s or ">" not in s:
+        print(n)
+    else:
+        for i in range(n):
+            if s[i] == '-':
+                ans.add(i)
+                ans.add((i + 1) % n)
+        print(len(ans))
